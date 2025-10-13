896e3fc2b9b52b3e3b2df8443c25c2d4f0962a1d
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-10-13 09:49:45 +0300

    feat: add TypeScript declarations for importing .vue files

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+/* Type declarations for importing .vue files in TypeScript */
+declare module '*.vue' {
+  import type {DefineComponent} from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
