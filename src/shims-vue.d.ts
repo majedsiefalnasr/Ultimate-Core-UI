87e763f2a0ba06984d9cbd2a1722591696f07d75
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-10-21 13:27:37 +0300

    refactor: remove eslint directive for consistent type definitions in shims-vue.d.ts

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
 /* Type declarations for importing .vue files in TypeScript */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
