f0222bf8912fea2e410154806028da071737492f
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-10-28 10:06:20 +0300

    feat: add transition components UExpandTransition, UFadeTransition, UScaleTransition, USlideXTransition, and USlideYTransition with templates and styles

---
+++ src/components/Transitions/UScaleTransition.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  /**
+   * UScaleTransition - scale transition component
+   *
+   * @component
+   * @extends VScaleTransition
+   */
+  import { VScaleTransition } from 'vuetify/components';
+  import './UScaleTransition.scss';
+
+  // Component options
+  defineOptions({ name: 'UScaleTransition', inheritAttrs: false });
+
+  // Define slots with generic props
+  defineSlots<{ [key: string]: (props: Record<string, unknown>) => unknown }>();
+</script>
+
+<template>
+  <v-scale-transition v-bind="$attrs">
+    <template v-for="(_, name) in $slots" :key="name" #[name]="slotData">
+      <slot :name="name" v-bind="slotData || {}" />
+    </template>
+  </v-scale-transition>
+</template>
+
+<style scoped lang="scss"></style>
