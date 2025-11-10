e36d732ad6a200435efcd0853c5939e789dd74b3
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-11-10 15:11:06 +0200

    feat(UDivider): enhance stories with additional props and vertical layout support

--- src/components/UDivider/UDivider.stories.ts
+++ src/components/UDivider/UDivider.stories.ts
@@ -1,5 +1,17 @@
 import type { Meta, StoryFn } from '@storybook/vue3';
 
+import {
+  UAppBarNavIcon,
+  UBtn,
+  UCard,
+  UList,
+  UListItem,
+  UListSubheader,
+  UToolbar,
+  UToolbarItems,
+  UToolbarTitle,
+} from '../index';
+
 import UDivider from './UDivider.vue';
 
 interface ComponentArgs {
@@ -7,16 +19,19 @@ interface ComponentArgs {
   vertical?: boolean;
   thickness?: string | number;
   color?: string;
+  length?: string | number;
+  opacity?: string | number;
+  theme?: string;
   class?: string;
 }
 
 const meta: Meta<ComponentArgs> = {
-  title: 'Component/Containment/Divider',
+  title: 'Components/Containment/Dividers',
   component: UDivider,
   parameters: {
     docs: {
       description: {
-        component: 'The UDivider component is used to separate sections of lists or layouts.',
+        component: 'The u-divider component is used to separate sections of lists or layouts.',
       },
       import: `import { UDivider } from '@ultimate/core-ui/components'`,
       source: {
@@ -34,7 +49,7 @@ const meta: Meta<ComponentArgs> = {
 
           const attrsString = attrsArray.length > 0 ? ' ' + attrsArray.join(' ') : '';
 
-          return `<UDivider${attrsString} />`;
+          return `<u-divider${attrsString}></u-divider>`;
         },
       },
     },
@@ -46,6 +61,14 @@ const meta: Meta<ComponentArgs> = {
       content:
         "This component is built on top of Vuetify's VDivider component. For detailed usage and props, refer to the Vuetify documentation linked below.",
       link: 'https://vuetifyjs.com/en/components/dividers/',
+    },
+    api: {
+      data: [
+        {
+          element: { title: 'v-divider', link: 'https://vuetifyjs.com/en/api/v-divider/' },
+          description: 'The divider component.',
+        },
+      ],
     },
   },
   argTypes: {
@@ -60,13 +83,28 @@ const meta: Meta<ComponentArgs> = {
       table: { type: { summary: 'boolean' }, defaultValue: { summary: 'false' } },
     },
     thickness: {
-      control: 'number',
-      description: 'Adjust the thickness of the divider (in px).',
-      table: { type: { summary: 'number | string' } },
+      control: 'text',
+      description: 'Sets the dividers thickness. Default unit is px.',
+      table: { type: { summary: 'string | number' } },
+    },
+    length: {
+      control: 'text',
+      description: 'Sets the dividers length. Default unit is px.',
+      table: { type: { summary: 'string | number' } },
+    },
+    opacity: {
+      control: 'text',
+      description: 'Sets the component’s opacity value',
+      table: { type: { summary: 'string | number' } },
     },
     color: {
       control: 'color',
       description: 'Theme color or CSS color to control appearance.',
+      table: { type: { summary: 'string' } },
+    },
+    theme: {
+      control: 'text',
+      description: 'Specify a theme for this component and all of its children.',
       table: { type: { summary: 'string' } },
     },
     class: {
@@ -84,7 +122,220 @@ export const Default: StoryFn<ComponentArgs> = (args) => ({
   setup() {
     return { args };
   },
-  template: '<UDivider v-bind="args" />',
+  template: '<u-divider v-bind="args" />',
 });
 
 Default.args = {} as ComponentArgs;
+
+// Inset Story
+export const Inset: StoryFn<ComponentArgs> = () => ({
+  components: { UCard, UList, UListItem, UListSubheader, UDivider },
+  template: `
+    <u-card class="mx-auto" max-width="425">
+      <u-list lines="two">
+        <u-list-subheader>Today</u-list-subheader>
+
+        <u-list-item
+          prepend-avatar="https://cdn.vuetifyjs.com/images/lists/1.jpg"
+          title="Brunch this weekend?"
+        >
+          <template #subtitle>
+            <span class="font-weight-bold">Ali Connors</span> — I'll be in your neighborhood doing errands this weekend. Do you want to hang out?
+          </template>
+        </u-list-item>
+
+        <u-divider inset></u-divider>
+
+        <u-list-item
+          prepend-avatar="https://cdn.vuetifyjs.com/images/lists/2.jpg"
+        >
+          <template #title>
+            Summer BBQ <span class="text-grey-lighten-1">4</span>
+          </template>
+
+          <template #subtitle>
+            <span class="font-weight-bold">to Alex, Scott, Jennifer</span> — Wish I could come, but I'm out of town this weekend.
+          </template>
+        </u-list-item>
+
+        <u-divider inset></u-divider>
+
+        <u-list-item
+          prepend-avatar="https://cdn.vuetifyjs.com/images/lists/3.jpg"
+          title="Oui oui"
+        >
+          <template #subtitle>
+            <span class="font-weight-bold">Sandra Adams</span> — Do you have Paris recommendations? Have you ever been?
+          </template>
+        </u-list-item>
+      </u-list>
+    </u-card>
+  `,
+});
+
+Inset.parameters = {
+  docs: {
+    description: {
+      story:
+        'Inset dividers are moved 72px to the right. This will cause them to line up with list items.',
+    },
+    source: {
+      code: `<template>
+  <u-card
+    class="mx-auto"
+    max-width="425"
+  >
+    <u-list lines="two">
+      <u-list-subheader>Today</u-list-subheader>
+
+      <u-list-item
+        prepend-avatar="https://cdn.vuetifyjs.com/images/lists/1.jpg"
+        title="Brunch this weekend?"
+      >
+        <template #subtitle>
+          <span class="font-weight-bold">Ali Connors</span> — I'll be in your neighborhood doing errands this weekend. Do you want to hang out?
+        </template>
+      </u-list-item>
+
+      <u-divider inset></u-divider>
+
+      <u-list-item
+        prepend-avatar="https://cdn.vuetifyjs.com/images/lists/2.jpg"
+      >
+        <template #title>
+          Summer BBQ <span class="text-grey-lighten-1">4</span>
+        </template>
+
+        <template #subtitle>
+          <span class="font-weight-bold">to Alex, Scott, Jennifer</span> — Wish I could come, but I'm out of town this weekend.
+        </template>
+      </u-list-item>
+
+      <u-divider inset></u-divider>
+
+      <u-list-item
+        prepend-avatar="https://cdn.vuetifyjs.com/images/lists/3.jpg"
+        title="Oui oui"
+      >
+        <template #subtitle>
+          <span class="font-weight-bold">Sandra Adams</span> — Do you have Paris recommendations? Have you ever been?
+        </template>
+      </u-list-item>
+    </u-list>
+  </u-card>
+</template>`,
+    },
+  },
+};
+
+// Vertical Story
+export const Vertical: StoryFn<ComponentArgs> = () => ({
+  components: {
+    UToolbar,
+    UToolbarTitle,
+    UToolbarItems,
+    UAppBarNavIcon,
+    UBtn,
+    UDivider,
+  },
+  template: `
+    <div class="w-100">
+      <div class="text-body-2 mb-1">dividers with <code>vertical</code></div>
+      <u-toolbar color="purple">
+        <template #prepend>
+          <div class="text-h5 px-3">Title</div>
+        </template>
+
+        <u-divider class="mx-3" vertical></u-divider>
+        <u-toolbar-title>My Home</u-toolbar-title>
+
+        <u-toolbar-items>
+          <u-btn variant="text">News</u-btn>
+          <u-divider vertical></u-divider>
+          <u-btn variant="text">Blog</u-btn>
+          <u-divider vertical></u-divider>
+          <u-btn variant="text">Music</u-btn>
+        </u-toolbar-items>
+        <u-divider vertical></u-divider>
+        <u-app-bar-nav-icon class="ms-2"></u-app-bar-nav-icon>
+      </u-toolbar>
+
+      <div class="text-body-2 mt-3 mb-1">dividers with <code>vertical</code> and <code>inset</code></div>
+      <u-toolbar color="teal">
+        <template #prepend>
+          <div class="text-h5 px-3">Title</div>
+        </template>
+
+        <u-divider class="mx-3" inset vertical></u-divider>
+        <u-toolbar-title>My Home</u-toolbar-title>
+
+        <u-toolbar-items>
+          <u-btn variant="text">News</u-btn>
+          <u-divider inset vertical></u-divider>
+          <u-btn variant="text">Blog</u-btn>
+          <u-divider inset vertical></u-divider>
+          <u-btn variant="text">Music</u-btn>
+        </u-toolbar-items>
+        <u-divider inset vertical></u-divider>
+        <u-app-bar-nav-icon class="ms-2"></u-app-bar-nav-icon>
+      </u-toolbar>
+    </div>
+  `,
+});
+
+Vertical.parameters = {
+  docs: {
+    description: {
+      story: 'Vertical dividers give you more tools for unique layouts.',
+    },
+    source: {
+      code: `<template>
+  <div class="text-body-2 mb-1">dividers with <code>vertical</code></div>
+  <u-toolbar color="purple">
+    <template #prepend>
+      <div class="text-h5 px-3">Title</div>
+    </template>
+
+    <u-divider class="mx-3" vertical></u-divider>
+    <u-toolbar-title>My Home</u-toolbar-title>
+
+    <u-toolbar-items>
+      <u-btn variant="text">News</u-btn>
+      <u-divider vertical></u-divider>
+      <u-btn variant="text">Blog</u-btn>
+      <u-divider vertical></u-divider>
+      <u-btn variant="text">Music</u-btn>
+    </u-toolbar-items>
+    <u-divider vertical></u-divider>
+    <u-app-bar-nav-icon class="ms-2"></u-app-bar-nav-icon>
+  </u-toolbar>
+
+  <div class="text-body-2 mt-3 mb-1">dividers with <code>vertical</code> and <code>inset</code></div>
+  <u-toolbar color="teal">
+    <template #prepend>
+      <div class="text-h5 px-3">Title</div>
+    </template>
+
+    <u-divider class="mx-3" inset vertical></u-divider>
+    <u-toolbar-title>My Home</u-toolbar-title>
+
+    <u-toolbar-items>
+      <u-btn variant="text">News</u-btn>
+      <u-divider inset vertical></u-divider>
+      <u-btn variant="text">Blog</u-btn>
+      <u-divider inset vertical></u-divider>
+      <u-btn variant="text">Music</u-btn>
+    </u-toolbar-items>
+    <u-divider inset vertical></u-divider>
+    <u-app-bar-nav-icon class="ms-2"></u-app-bar-nav-icon>
+  </u-toolbar>
+</template>
+
+<style scoped>
+.v-toolbar .v-divider {
+  --v-border-opacity: .7
+}
+</style>`,
+    },
+  },
+};
