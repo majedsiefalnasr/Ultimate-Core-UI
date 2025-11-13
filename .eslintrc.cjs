43641e19071d6b6a7521bba2bce1e3aecc75fc1a
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-11-13 13:03:13 +0200

    fix(eslint): change '@typescript-eslint/no-explicit-any' rule from error to warn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,7 +27,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'vue/script-setup-uses-vars': 'error',
     // TypeScript
-    '@typescript-eslint/no-explicit-any': ['error'],
+    '@typescript-eslint/no-explicit-any': ['warn'],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
     // Best practices
