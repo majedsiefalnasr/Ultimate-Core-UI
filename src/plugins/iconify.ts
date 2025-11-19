31579b13ee35b911c2345ae7e05971dd4fee278f
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-11-19 16:13:43 +0200

    feat(iconify): update rating icons to use SVG paths for improved rendering

--- src/plugins/iconify.ts
+++ src/plugins/iconify.ts
@@ -29,9 +29,13 @@ const aliases: IconAliases = {
   radioOn: 'hugeicons:stop-circle',
   radioOff: 'hugeicons:circle',
   edit: 'hugeicons:pencil-edit-01',
-  ratingEmpty: 'hugeicons:star-off',
-  ratingFull: 'hugeicons:star',
-  ratingHalf: 'hugeicons:star-half',
+  ratingEmpty:
+    'svg:M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z',
+  ratingFull:
+    'svg:M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z',
+  ratingHalf:
+    'svg:M12,15.4V6.1L13.71,10.13L18.09,10.5L14.77,13.39L15.76,17.67M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z',
+
   loading: 'hugeicons:loading-03',
   first: 'hugeicons:arrow-left-03',
   last: 'hugeicons:arrow-right-03',
