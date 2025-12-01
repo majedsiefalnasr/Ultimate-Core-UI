0b351dba3e0c47eb3a5a1b36a4933a4b6dd91c9c
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-12-01 10:35:34 +0200

    feat(UHover): improve story documentation and refactor templates for clarity

--- src/components/UHover/UHover.stories.ts
+++ src/components/UHover/UHover.stories.ts
@@ -119,59 +119,30 @@ const meta: Meta<ComponentArgs> = {
 
 export default meta;
 
+// Default Story
 export const Default: StoryFn<ComponentArgs> = (args) => ({
   components: { UHover, UCard },
   setup() {
     return { args };
   },
   template: `
-    <u-hover v-bind="args">
-      <template v-slot:default="{ isHovering, props }">
-        <u-card
-          v-bind="props"
-          :color="isHovering ? 'primary' : undefined"
-          title="Hover over me"
-          text="Move your cursor over this card to see the hover effect in action."
-        ></u-card>
-      </template>
-    </u-hover>
-  `,
+  <u-hover v-bind="args">
+    <template v-slot:default="{ isHovering, props }">
+      <u-card
+        v-bind="props"
+        :color="isHovering ? 'primary' : undefined"
+        title="Hover over me"
+        text="Move your cursor over this card to see the hover effect in action."
+      ></u-card>
+    </template>
+  </u-hover>
+`,
 });
 
 Default.args = {} as ComponentArgs;
 
-export const Disabled: StoryFn<ComponentArgs> = (args) => ({
-  components: { UHover, UCard, UCardText, URow, UCol },
-  setup() {
-    return { args };
-  },
-  template: `
-    <u-row align="center" justify="center">
-      <u-col cols="12">
-        <u-hover v-slot="{ isHovering, props }" disabled>
-          <u-card
-            :elevation="isHovering ? 12 : 2"
-            class="mx-auto"
-            height="350"
-            max-width="350"
-            v-bind="props"
-          >
-            <u-card-text class="my-4 text-center text-h6">
-              Hover over me!
-            </u-card-text>
-          </u-card>
-        </u-hover>
-      </u-col>
-    </u-row>
-  `,
-});
-
-Disabled.args = {} as ComponentArgs;
-
-Disabled.parameters = {
-  docs: {
-    source: {
-      code: `<template>
+// Disabled Story
+const disabledTemplate = `
   <u-row align="center" justify="center">
     <u-col cols="12">
       <u-hover v-slot="{ isHovering, props }" disabled>
@@ -189,64 +160,26 @@ Disabled.parameters = {
       </u-hover>
     </u-col>
   </u-row>
-</template>`,
-    },
-    description: {
-      story: 'The disabled prop disables the hover functionality.',
+`;
+
+/**
+ * The disabled prop disables the hover functionality.
+ */
+export const Disabled: StoryFn<ComponentArgs> = () => ({
+  components: { UHover, UCard, UCardText, URow, UCol },
+  template: disabledTemplate,
+});
+
+Disabled.parameters = {
+  docs: {
+    source: {
+      code: `<template>${disabledTemplate}</template>`,
     },
   },
 };
 
-export const OpenAndCloseDelay: StoryFn<ComponentArgs> = (args) => ({
-  components: { UHover, UCard, UCardText, URow, UCol },
-  setup() {
-    return { args };
-  },
-  template: `
-    <u-row>
-      <u-col cols="12" sm="6">
-        <u-hover v-slot="{ isHovering, props }" open-delay="200">
-          <u-card
-            :class="{ 'on-hover': isHovering }"
-            :elevation="isHovering ? 16 : 2"
-            class="mx-auto"
-            height="350"
-            max-width="350"
-            v-bind="props"
-          >
-            <u-card-text class="font-weight-medium mt-12 text-center text-subtitle-1">
-              Open Delay (Mouse enter)
-            </u-card-text>
-          </u-card>
-        </u-hover>
-      </u-col>
-
-      <u-col cols="12" sm="6">
-        <u-hover v-slot="{ isHovering, props }" close-delay="200">
-          <u-card
-            :class="{ 'on-hover': isHovering }"
-            :elevation="isHovering ? 16 : 2"
-            class="mx-auto"
-            height="350"
-            max-width="350"
-            v-bind="props"
-          >
-            <u-card-text class="font-weight-medium mt-12 text-center text-subtitle-1">
-              Close Delay (Mouse leave)
-            </u-card-text>
-          </u-card>
-        </u-hover>
-      </u-col>
-    </u-row>
-  `,
-});
-
-OpenAndCloseDelay.args = {} as ComponentArgs;
-
-OpenAndCloseDelay.parameters = {
-  docs: {
-    source: {
-      code: `<template>
+// Open And Close Delay Story
+const openAndCloseDelayTemplate = `
   <u-row>
     <u-col cols="12" sm="6">
       <u-hover v-slot="{ isHovering, props }" open-delay="200">
@@ -282,95 +215,34 @@ OpenAndCloseDelay.parameters = {
       </u-hover>
     </u-col>
   </u-row>
-</template>
+`;
+
+/**
+ * Delay u-hover events by using open-delay and close-delay props in combination or separately.
+ */
+export const OpenAndCloseDelay: StoryFn<ComponentArgs> = () => ({
+  components: { UHover, UCard, UCardText, URow, UCol },
+  template: openAndCloseDelayTemplate,
+});
+
+OpenAndCloseDelay.parameters = {
+  docs: {
+    source: {
+      code: `<template>${openAndCloseDelayTemplate}</template>
+
 <style lang="sass" scoped>
-.v-card.on-hover.v-theme--dark
-  background-color: rgba(#FFF, 0.8)
-  >.v-card__text
-    color: #000
+  .v-card.on-hover.v-theme--dark
+    background-color: rgba(#FFF, 0.8)
+    >.v-card__text
+      color: #000
 </style>`,
-    },
-    description: {
-      story:
-        'Delay u-hover events by using open-delay and close-delay props in combination or separately.',
     },
   },
 };
 
-export const HoverList: StoryFn<ComponentArgs> = (args) => ({
-  components: { UHover, UCard, UCardText, URow, UCol, UContainer, UImg, UBtn },
-  setup() {
-    const icons = ['hugeicons:arrow-left-double', 'hugeicons:play', 'hugeicons:arrow-right-double'];
-    const items = [
-      {
-        title: 'New Releases',
-        text: "It's New Release Friday",
-        subtext: 'Newly released songs.',
-        img: 'https://cdn.vuetifyjs.com/docs/images/cards/hands.jpg',
-      },
-      {
-        title: 'Rock',
-        text: 'Greatest Rock Hits',
-        subtext: 'Lose yourself in rock tunes.',
-        img: 'https://cdn.vuetifyjs.com/docs/images/cards/singer.jpg',
-      },
-      {
-        title: 'Mellow Moods',
-        text: 'Ambient Bass',
-        subtext: 'Chill beats to mellow you out.',
-        img: 'https://cdn.vuetifyjs.com/docs/images/cards/concert.jpg',
-      },
-    ];
-    const transparent = 'rgba(255, 255, 255, 0)';
-
-    return { args, icons, items, transparent };
-  },
-  template: `
-    <u-container id="hover-list" class="pa-4 text-center">
-      <u-row align="center" class="fill-height" justify="center">
-        <template v-for="(item, i) in items" :key="i">
-          <u-col cols="12" md="4">
-            <u-hover v-slot="{ isHovering, props }">
-              <u-card
-                :class="{ 'on-hover': isHovering }"
-                :elevation="isHovering ? 12 : 2"
-                v-bind="props"
-              >
-                <u-img :src="item.img" height="225px" cover>
-                  <u-card-text class="text-h6 text-white d-flex flex-column">
-                    <p class="mt-4">{{ item.title }}</p>
-                    <div>
-                      <p class="ma-0 text-body-1 font-weight-bold">{{ item.text }}</p>
-                      <p class="text-caption font-weight-medium">{{ item.subtext }}</p>
-                    </div>
-                  </u-card-text>
-                  <div class="align-self-center">
-                    <u-btn
-                      v-for="(icon, index) in icons"
-                      :key="index"
-                      :class="{ 'show-btns': isHovering }"
-                      :color="transparent"
-                      :icon="icon"
-                      variant="text"
-                    ></u-btn>
-                  </div>
-                </u-img>
-              </u-card>
-            </u-hover>
-          </u-col>
-        </template>
-      </u-row>
-    </u-container>
-  `,
-});
-
-HoverList.args = {} as ComponentArgs;
-
-HoverList.parameters = {
-  docs: {
-    source: {
-      code: `<template>
-  <u-container class="pa-4 text-center">
+// Hover List Story
+const hoverListTemplate = `
+  <u-container id="hover-list" class="pa-4 text-center">
     <u-row align="center" class="fill-height" justify="center">
       <template v-for="(item, i) in items" :key="i">
         <u-col cols="12" md="4">
@@ -405,7 +277,48 @@ HoverList.parameters = {
       </template>
     </u-row>
   </u-container>
-</template>
+`;
+
+/**
+ * u-hover can be used in combination with v-for to make a single item stand out when the
+ * user interacts with the list.
+ */
+export const HoverList: StoryFn<ComponentArgs> = () => ({
+  components: { UHover, UCard, UCardText, URow, UCol, UContainer, UImg, UBtn },
+  setup() {
+    const icons = ['hugeicons:arrow-left-double', 'hugeicons:play', 'hugeicons:arrow-right-double'];
+    const items = [
+      {
+        title: 'New Releases',
+        text: "It's New Release Friday",
+        subtext: 'Newly released songs.',
+        img: 'https://cdn.vuetifyjs.com/docs/images/cards/hands.jpg',
+      },
+      {
+        title: 'Rock',
+        text: 'Greatest Rock Hits',
+        subtext: 'Lose yourself in rock tunes.',
+        img: 'https://cdn.vuetifyjs.com/docs/images/cards/singer.jpg',
+      },
+      {
+        title: 'Mellow Moods',
+        text: 'Ambient Bass',
+        subtext: 'Chill beats to mellow you out.',
+        img: 'https://cdn.vuetifyjs.com/docs/images/cards/concert.jpg',
+      },
+    ];
+    const transparent = 'rgba(255, 255, 255, 0)';
+
+    return { icons, items, transparent };
+  },
+  template: hoverListTemplate,
+});
+
+HoverList.parameters = {
+  docs: {
+    source: {
+      code: `<template>${hoverListTemplate}</template>
+
 <script setup>
   const icons = [
     'hugeicons:arrow-left-double-01',
@@ -448,70 +361,12 @@ HoverList.parameters = {
   }
 </style>`,
     },
-    description: {
-      story:
-        'u-hover can be used in combination with v-for to make a single item stand out when the user interacts with the list.',
-    },
   },
 };
 
-export const Transition: StoryFn<ComponentArgs> = (args) => ({
-  components: { UHover, UCard, UCardText, UImg, UExpandTransition },
-  setup() {
-    return { args };
-  },
-  template: `
-    <div id="hover-transition">
-      <u-hover v-slot="{ isHovering, props }">
-        <u-card
-          class="mx-auto"
-          color="grey-lighten-4"
-          max-width="600"
-          v-bind="props"
-        >
-          <u-img
-            :aspect-ratio="16/9"
-            src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
-            cover
-          >
-            <u-expand-transition>
-              <div
-                v-if="isHovering"
-                class="d-flex bg-orange-darken-2 v-card--reveal text-h2"
-                style="height: 100%;"
-              >
-                $14.99
-              </div>
-            </u-expand-transition>
-          </u-img>
-
-          <u-card-text class="pt-6">
-            <div class="font-weight-light text-grey text-h6 mb-2">
-              For the perfect meal
-            </div>
-
-            <h3 class="text-h4 font-weight-light text-orange mb-2">
-              QW cooking utensils
-            </h3>
-
-            <div class="font-weight-light text-h6 mb-2">
-              Our Vintage kitchen utensils delight any chef.<br>
-              Made of bamboo by hand
-            </div>
-          </u-card-text>
-        </u-card>
-      </u-hover>
-    </div>
-  `,
-});
-
-Transition.args = {} as ComponentArgs;
-
-Transition.parameters = {
-  docs: {
-    source: {
-      code: `<template>
-  <div>
+// Transition Story
+const transitionTemplate = `
+  <div id="hover-transition">
     <u-hover v-slot="{ isHovering, props }">
       <u-card
         class="mx-auto"
@@ -552,7 +407,21 @@ Transition.parameters = {
       </u-card>
     </u-hover>
   </div>
-</template>
+`;
+
+/**
+ * Create highly customized components that respond to user interaction.
+ */
+export const Transition: StoryFn<ComponentArgs> = () => ({
+  components: { UHover, UCard, UCardText, UImg, UExpandTransition },
+  template: transitionTemplate,
+});
+
+Transition.parameters = {
+  docs: {
+    source: {
+      code: `<template>${transitionTemplate}</template>
+
 <style scoped>
   .v-card--reveal {
     align-items: center;
@@ -563,9 +432,6 @@ Transition.parameters = {
     width: 100%;
   }
 </style>`,
-    },
-    description: {
-      story: 'Create highly customized components that respond to user interaction.',
     },
   },
 };
