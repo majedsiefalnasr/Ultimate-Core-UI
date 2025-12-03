97139675561c95c6ed51e4053f96491a3b4905b5
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-12-03 11:17:01 +0200

    feat(ULayout): refactor story templates for improved clarity and structure

--- src/components/ULayout/ULayout.stories.ts
+++ src/components/ULayout/ULayout.stories.ts
@@ -42,28 +42,54 @@ const meta: Meta<ComponentArgs> = {
           const attrsString = attrsArray.length > 0 ? ' ' + attrsArray.join(' ') : '';
 
           return `
-    <u-layout${attrsString} class="rounded rounded-md border">
-      <u-app-bar title="Application bar"></u-app-bar>
+  <u-layout${attrsString} class="rounded rounded-md">
+    <u-system-bar color="grey-darken-3"></u-system-bar>
 
-      <u-navigation-drawer>
-        <u-list nav>
-          <u-list-item title="Navigation drawer" link></u-list-item>
-        </u-list>
-      </u-navigation-drawer>
+    <u-navigation-drawer
+      color="grey-darken-2"
+      width="72"
+      permanent
+    ></u-navigation-drawer>
+    
+    <u-navigation-drawer
+      color="grey-darken-1"
+      width="150"
+      permanent
+    ></u-navigation-drawer>
 
-      <u-main class="d-flex align-center justify-center" height="300">
-        <u-container>
-          <u-sheet
-            border="dashed md"
-            color="surface-light"
-            height="200"
-            rounded="lg"
-            width="100%"
-          ></u-sheet>
-        </u-container>
-      </u-main>
-    </u-layout>
-          `;
+    <u-app-bar
+      color="grey"
+      height="48"
+      flat
+    ></u-app-bar>
+    
+    <u-navigation-drawer
+      color="grey-lighten-1"
+      location="right"
+      width="150"
+      permanent
+    ></u-navigation-drawer>
+
+    <u-app-bar
+      color="grey-lighten-2"
+      height="48"
+      location="bottom"
+      flat
+    ></u-app-bar>
+
+    <u-main class="d-flex align-center justify-center" height="300">
+      <u-container>
+        <u-sheet
+          border="dashed md"
+          color="surface-light"
+          height="150"
+          rounded="lg"
+          width="100%"
+        ></u-sheet>
+      </u-container>
+    </u-main>
+  </u-layout>
+`;
         },
       },
     },
@@ -83,146 +109,47 @@ const meta: Meta<ComponentArgs> = {
 
 export default meta;
 
+// Default Story
 export const Default: StoryFn<ComponentArgs> = (args) => ({
   components: { ULayout, UAppBar, UNavigationDrawer, UMain, UContainer, USheet },
   setup() {
     return { args };
   },
   template: `
-    <u-layout class="rounded rounded-md">
-      <u-system-bar color="grey-darken-3"></u-system-bar>
-
-      <u-navigation-drawer
-        color="grey-darken-2"
-        width="72"
-        permanent
-      ></u-navigation-drawer>
-      
-      <u-navigation-drawer
-        color="grey-darken-1"
-        width="150"
-        permanent
-      ></u-navigation-drawer>
-
-      <u-app-bar
-        color="grey"
-        height="48"
-        flat
-      ></u-app-bar>
-      
-      <u-navigation-drawer
-        color="grey-lighten-1"
-        location="right"
-        width="150"
-        permanent
-      ></u-navigation-drawer>
-
-      <u-app-bar
-        color="grey-lighten-2"
-        height="48"
-        location="bottom"
-        flat
-      ></u-app-bar>
-
-      <u-main class="d-flex align-center justify-center" height="300">
-        <u-container>
-          <u-sheet
-            border="dashed md"
-            color="surface-light"
-            height="150"
-            rounded="lg"
-            width="100%"
-          ></u-sheet>
-        </u-container>
-      </u-main>
-    </u-layout>
-  `,
-});
-
-Default.args = {} as ComponentArgs;
-
-export const ComplexLayouts: StoryFn<ComponentArgs> = (args) => ({
-  components: {
-    ULayout,
-    USystemBar,
-    UNavigationDrawer,
-    UAppBar,
-    UMain,
-    UContainer,
-    USheet,
-  },
-  setup() {
-    return { args };
-  },
-  template: `
-    <u-layout class="rounded rounded-md">
-      <u-system-bar color="grey-darken-3"></u-system-bar>
-
-      <u-navigation-drawer
-        color="grey-darken-2"
-        width="72"
-        permanent
-      ></u-navigation-drawer>
-      
-      <u-navigation-drawer
-        color="grey-darken-1"
-        width="150"
-        permanent
-      ></u-navigation-drawer>
-
-      <u-app-bar
-        color="grey"
-        height="48"
-        flat
-      ></u-app-bar>
-      
-      <u-navigation-drawer
-        color="grey-lighten-1"
-        location="right"
-        width="150"
-        permanent
-      ></u-navigation-drawer>
-
-      <u-app-bar
-        color="grey-lighten-2"
-        height="48"
-        location="bottom"
-        flat
-      ></u-app-bar>
-
-      <u-main class="d-flex align-center justify-center" height="300">
-        <u-container>
-          <u-sheet
-            border="dashed md"
-            color="surface-light"
-            height="150"
-            rounded="lg"
-            width="100%"
-          ></u-sheet>
-        </u-container>
-      </u-main>
-    </u-layout>
-  `,
-});
-
-ComplexLayouts.args = {} as ComponentArgs;
-
-ComplexLayouts.parameters = {
-  docs: {
-    source: {
-      code: `<template>
   <u-layout class="rounded rounded-md">
     <u-system-bar color="grey-darken-3"></u-system-bar>
 
-    <u-navigation-drawer color="grey-darken-2" width="72" permanent></u-navigation-drawer>
+    <u-navigation-drawer
+      color="grey-darken-2"
+      width="72"
+      permanent
+    ></u-navigation-drawer>
+    
+    <u-navigation-drawer
+      color="grey-darken-1"
+      width="150"
+      permanent
+    ></u-navigation-drawer>
 
-    <u-navigation-drawer color="grey-darken-1" width="150" permanent></u-navigation-drawer>
+    <u-app-bar
+      color="grey"
+      height="48"
+      flat
+    ></u-app-bar>
+    
+    <u-navigation-drawer
+      color="grey-lighten-1"
+      location="right"
+      width="150"
+      permanent
+    ></u-navigation-drawer>
 
-    <u-app-bar color="grey" height="48" flat></u-app-bar>
-
-    <u-navigation-drawer color="grey-lighten-1" location="right" width="150" permanent></u-navigation-drawer>
-
-    <u-app-bar color="grey-lighten-2" height="48" location="bottom" flat></u-app-bar>
+    <u-app-bar
+      color="grey-lighten-2"
+      height="48"
+      location="bottom"
+      flat
+    ></u-app-bar>
 
     <u-main class="d-flex align-center justify-center" height="300">
       <u-container>
@@ -236,43 +163,88 @@ ComplexLayouts.parameters = {
       </u-container>
     </u-main>
   </u-layout>
-</template>
-<script setup lang="ts"></script>`,
+`,
+});
+
+Default.args = {} as ComponentArgs;
+
+// ComplexLayouts Story
+const complexLayoutsTemplate = `
+  <u-layout class="rounded rounded-md">
+    <u-system-bar color="grey-darken-3"></u-system-bar>
+
+    <u-navigation-drawer
+      color="grey-darken-2"
+      width="72"
+      permanent
+    ></u-navigation-drawer>
+    
+    <u-navigation-drawer
+      color="grey-darken-1"
+      width="150"
+      permanent
+    ></u-navigation-drawer>
+
+    <u-app-bar
+      color="grey"
+      height="48"
+      flat
+    ></u-app-bar>
+    
+    <u-navigation-drawer
+      color="grey-lighten-1"
+      location="right"
+      width="150"
+      permanent
+    ></u-navigation-drawer>
+
+    <u-app-bar
+      color="grey-lighten-2"
+      height="48"
+      location="bottom"
+      flat
+    ></u-app-bar>
+
+    <u-main class="d-flex align-center justify-center" height="300">
+      <u-container>
+        <u-sheet
+          border="dashed md"
+          color="surface-light"
+          height="150"
+          rounded="lg"
+          width="100%"
+        ></u-sheet>
+      </u-container>
+    </u-main>
+  </u-layout>
+`;
+
+/**
+ * Complex layout with multiple navigation drawers and app bars.
+ */
+export const ComplexLayouts: StoryFn<ComponentArgs> = () => ({
+  components: {
+    ULayout,
+    USystemBar,
+    UNavigationDrawer,
+    UAppBar,
+    UMain,
+    UContainer,
+    USheet,
+  },
+  template: complexLayoutsTemplate,
+});
+
+ComplexLayouts.parameters = {
+  docs: {
+    source: {
+      code: `<template>${complexLayoutsTemplate}</template>`,
     },
   },
 };
 
-export const DynamicOrder: StoryFn<ComponentArgs> = (args) => ({
-  components: { ULayout, UNavigationDrawer, UAppBar, UMain, UContainer, USheet, USwitch },
-  setup() {
-    const order = shallowRef(0);
-    return { args, order };
-  },
-  template: `
-    <u-layout class="rounded rounded-md border" v-bind="args">
-      <u-navigation-drawer color="surface-variant" permanent></u-navigation-drawer>
-
-      <u-app-bar :order="order" color="grey-lighten-2" title="Application bar" flat>
-        <template #append>
-          <u-switch v-model="order" class="me-2" :false-value="0" :true-value="-1" label="Toggle order" inset hide-details></u-switch>
-        </template>
-      </u-app-bar>
-
-      <u-main class="d-flex align-center justify-center" height="300">
-        <u-container>
-          <u-sheet border="dashed md" color="surface-light" height="200" rounded="lg" width="100%"></u-sheet>
-        </u-container>
-      </u-main>
-    </u-layout>
-  `,
-});
-
-DynamicOrder.args = {} as ComponentArgs;
-
-DynamicOrder.parameters = {
-  docs: {
-    source: {
-      code: `<template>
+// DynamicOrder Story
+const dynamicOrderTemplate = `
   <u-layout class="rounded rounded-md border">
     <u-navigation-drawer color="surface-variant" permanent></u-navigation-drawer>
 
@@ -284,21 +256,33 @@ DynamicOrder.parameters = {
 
     <u-main class="d-flex align-center justify-center" height="300">
       <u-container>
-        <u-sheet
-          border="dashed md"
-          color="surface-light"
-          height="200"
-          rounded="lg"
-          width="100%"
-        ></u-sheet>
+        <u-sheet border="dashed md" color="surface-light" height="200" rounded="lg" width="100%"></u-sheet>
       </u-container>
     </u-main>
   </u-layout>
-</template>
-<script setup>
-import { shallowRef } from 'vue'
+`;
 
-const order = shallowRef(0)
+/**
+ * Dynamic layout with controllable app bar order.
+ */
+export const DynamicOrder: StoryFn<ComponentArgs> = () => ({
+  components: { ULayout, UNavigationDrawer, UAppBar, UMain, UContainer, USheet, USwitch },
+  setup() {
+    const order = shallowRef(0);
+    return { order };
+  },
+  template: dynamicOrderTemplate,
+});
+
+DynamicOrder.parameters = {
+  docs: {
+    source: {
+      code: `<template>${dynamicOrderTemplate}</template>
+
+<script setup>
+  import { shallowRef } from 'vue'
+
+  const order = shallowRef(0)
 </script>`,
     },
   },
